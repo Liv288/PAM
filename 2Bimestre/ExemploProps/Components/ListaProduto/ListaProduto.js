91a4b5358c15c9f53836e141ecee23d8ebd8bb58
Liv288 <liveira026@gmail.com>
2025-08-02 19:24:12 -0300

    Add files via upload

---
+++ 2Bimestre/ExemploProps/Components/ListaProduto/ListaProduto.js
@@ -0,0 +1,13 @@
+import {Text, View } from 'react-native';
+
+import Style from './Style';
+
+export default function ListaProduto(props) {
+    return (
+      <View style={Style.container}>
+        
+        <Text> Exemplo com FlatList </Text>        
+       
+      </View>
+    );
+  }
---
+++ 2Bimestre/ExemploProps/Components/ListaProduto/Style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 0.5, 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor:'#00f'
+    },
+  });
